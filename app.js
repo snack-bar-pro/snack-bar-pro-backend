177f692b7524f503a42fd06e2a7b13ed72a6716c
Alan <alan.liu@cargosmart.com>
2020-12-29 12:28:25 +0800

    add schedule to scan ros connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('./express')
 const config = require('./lib/config')
 const mongodb = require('./mongo_init')
 const mongoose = require('./src/util/mongoose.util')
+const checkRosConnectionSchedule = require('./src/schedule/checkRosConnection.schedule');
 
 async function initDb (){
   await mongodb.connect();
@@ -11,6 +12,7 @@ async function initDb (){
 module.exports.init = async () => {
   config.init();
   // const app = await initDb();
+  checkRosConnectionSchedule.checkRosConnection();
   const app = express.init();
   console.log('Server running in Port: 3001')
   app.listen(3001)
